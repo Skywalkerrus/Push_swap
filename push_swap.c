9149c06a270414df271ba6953b601a57b0b092e6
Bail Antario <bantario@mi-d5.21-school.ru>
2020-01-21 20:03:15 +0300

    pb. какие-то странные лишние нули

--- push_swap.c
+++ push_swap.c
@@ -6,40 +6,23 @@
 /*   By: bantario <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/10 17:48:53 by bantario          #+#    #+#             */
-/*   Updated: 2020/01/21 17:24:42 by bantario         ###   ########.fr       */
+/*   Updated: 2020/01/21 20:02:15 by bantario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-void	pa(t_stack *a,	t_stack *b)
+/*void	pa(t_stack *a,	t_stack *b)
 {
-	t_stack 	*new_elem;
 
-	if (a->num > 0)
-	{
-		new_elem = (t_stack *)malloc(sizeof(t_stack));
-		a->num++;
-		a->pred = new_elem;
-		new_elem->pred = NULL;
-		new_elem->next = a;
-		new_elem->value = b->value;
-	}
-	else if (a->num == 0)
-	{
-		a->value = b->value;
-		a->pred = NULL;
-		a->next = NULL;
-		a->num++;
-	}
-}
+} */
 
-t_stack		*sa_sb(t_stack *a, int i)
+t_stack		*sa_sb(t_stack *a)
 {
 	t_stack *c;
 
-	if (i > 1)
+	if (a->next != NULL)
 	{
 		c = a;
 		a = a->next;
@@ -49,12 +32,12 @@ t_stack		*sa_sb(t_stack *a, int i)
 	return (a);
 }
 
-/*void	ss(int *a, int *b, int i, int c)
+void	ss(t_stack *a, t_stack *b)
 {
-	sa_sb(a, i);
-	sa_sb(b, c);
+	sa_sb(a);
+	sa_sb(b);
 }
-*/
+
 
 void	print_stack(t_stack *a)
 {
@@ -64,44 +47,42 @@ void	print_stack(t_stack *a)
 		printf("\n");
 		return;
 	}
-	while (a->next != NULL)
+	while (a != NULL)
 	{
 		printf("stack: %d\n", a->value);
+		if (a->next == NULL)
+			break;
+		//printf("stack: %d\n", a->value);
 		a = a->next;
 	}
-	printf("\n");
+	printf("\n\n");
 }
 
-void	pb(t_stack *a, t_stack *b)
+t_stack		*pb(t_stack *a, t_stack *b)
 {
+	t_stack *c;
+
+	c = a->next;
 	if (b->value > 0)
 	{
-		printf("piska\n");
-		//while (b->pred != NULL)
-		//	b = b->pred;
-		//a->next->pred = NULL;
 		a->next = b;
-		b->pred = a;
-		b->pred->value = a->value;
-		b = b->pred;
+		c->pred = a;
 	}
 	if (b->value == 0)
 	{
-		printf("jopka\n");
-		a->next->pred = NULL;
 		a->next = NULL;
+		b = a;
 		b->value = a->value;
+		c->pred = b;
 	}
+	return (c);
 }
 
 int		main(int ac, char **av)
 {
-	//int	*a;
-	//int *b;
 	int c;
 	int i;
 	t_stack *a;
-	//t_stack *pred;
 	t_stack *ne;
 	t_stack *b;
 	t_stack *start;
@@ -109,7 +90,6 @@ int		main(int ac, char **av)
 	a = (t_stack*)malloc(sizeof(t_stack));
 	b = (t_stack*)malloc(sizeof(t_stack));
 	b->next = NULL;
-	b->pred = NULL;
 	i = 0;
 	c = 0;
 	if (ac > 1)
@@ -117,6 +97,7 @@ int		main(int ac, char **av)
 		while (av[i + 1])
 		{
 			a->value = ft_atoi(av[i + 1]);
+			printf("a->VALUE: %D\n", a->value);
 			a->num = i;
 			if (i == 0)
 				start = a;
@@ -125,22 +106,25 @@ int		main(int ac, char **av)
 			a = a->next;
 			ne->next = NULL;
 			i++;
-		}
-		
+		}	
 		av[i] = 0;
+
 		ft_putstr("stack a.\n");
 		print_stack(start);
-		/*pb(start, b);
+
+		start = pb(start, b);
+		b = start->pred;
+
+		start = pb(start, b);
+		b = start->pred;
+
+		printf("b->value: %d\n, b->next->value: %d\n", b->value, b->next->value);
+		ft_putstr("stack b.\n");
 		print_stack(b);
-		print_stack(start->next);
-		pb(start, b);
-		ft_putstr("stack b\n");
-		print_stack(b);
-		print_stack(start); */
-		start = sa_sb(start, ac);
-		//start = start->pred; // нужно делать после sa/sb
-		//print_stack(start);
-		print_stack(start);	
+
+		ft_putstr("stack a.\n");
+		printf("b->value: %d\n, b->next->value: %d\n", start->value, start->next->value);
+		print_stack(start);
 	}
 	return (0);
 }
